02315fb9c4f16d2eca3d10a2b10092251dc77650
willitcode <91231142+willitcode@users.noreply.github.com>
2024-01-16 15:23:32 -0600

    Add Spotless

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,6 +2,12 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+/*
+ * Asimov's Laws:
+ * The First Law: A robot may not injure a human being or, through inaction, allow a human being to come to harm.
+ * The Second Law: A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.
+ * The Third Law: A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
+ */
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
