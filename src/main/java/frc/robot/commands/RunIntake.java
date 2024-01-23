ff2e12c078c2abd06e948523c3168162d06c28f7
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-01-23 16:36:05 -0600

    Intake command stops and starts
    Added intake.runIntake() and intake.stop() to their respective methods.

--- src/main/java/frc/robot/commands/RunIntake.java
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -19,7 +19,9 @@ public class RunIntake extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    intake.runIntake();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -27,7 +29,9 @@ public class RunIntake extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    intake.stop();
+  }
 
   // Returns true when the command should end.
   @Override
