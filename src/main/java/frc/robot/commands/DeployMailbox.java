9dd26e5945b5834f4b528299c6b29c9fff901263
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-01-25 08:26:27 -0600

    removed unnecessary comments

--- src/main/java/frc/robot/commands/DeployMailbox.java
+++ src/main/java/frc/robot/commands/DeployMailbox.java
@@ -14,9 +14,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.RobotContainer;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+
 public class DeployMailbox extends ParallelCommandGroup {
   /** Creates a new DeployMailbox. */
   public DeployMailbox() {
