cb9a4e44bd6421f8dfb95affdabc47dec2406d86
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-01-24 18:42:25 -0600

    renamed belt subsystem to mailbox belts

--- src/main/java/frc/robot/subsystems/mailbox/MailboxBelts.java
+++ src/main/java/frc/robot/subsystems/mailbox/MailboxBelts.java
@@ -16,16 +16,18 @@ import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class Belt extends SubsystemBase {
+/** Subsystem for the mailbox belt. */
+public class MailboxBelts extends SubsystemBase {
   private CANSparkMax beltMotor;
 
-  public Belt() {
-    beltMotor = new CANSparkMax(Constants.Belt.BELT_DEVICE_ID, MotorType.kBrushless);
+  /** Create an object called "beltMotor" */
+  public MailboxBelts() {
+    beltMotor = new CANSparkMax(Constants.MailboxBelt.BELT_MOTOR_ID, MotorType.kBrushless);
   }
 
   /** Runs the belt. */
   public void runBelt() {
-    beltMotor.set(Constants.Belt.BELT_MOTOR_SPEED);
+    beltMotor.set(Constants.MailboxBelt.BELT_MOTOR_SPEED);
   }
 
   /** Stops the belt. */
