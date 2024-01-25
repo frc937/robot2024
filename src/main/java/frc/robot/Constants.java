e7daf2fc246127a545c569dc5b11b1819e5925f1
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-01-24 18:38:16 -0600

    changed to double solenoids and wrote javadoc

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -20,17 +20,27 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class Pneumatics {
-    public static final int LEFT_SOLENOID_CHANNEL = 0;
-    public static final int RIGHT_SOLENOID_CHANNEL = 1;
+  /** Constants for the Pneumatics system. */
+  public static class MailboxPneumatics {
+    /** The channel on the PCM for the forward direction on the left solenoid. */
+    public static final int LEFT_SOLENOID_FORWARD_CHANNEL = 0;
+    /** The channel on the PCM for the reverse direction on the left solenoid. */
+    public static final int LEFT_SOLENOID_REVERSE_CHANNEL = 1;
+    /** The channel on the PCM for the forward direction on the right solenoid. */
+    public static final int RIGHT_SOLENOID_FORWARD_CHANNEL = 2;
+    /** The channel on the PCM for the reverse direction on the right solenoid. */
+    public static final int RIGHT_SOLENOID_REVERSE_CHANNEL = 3;
   }
-
-  public static class Belt {
-    public static final int BELT_DEVICE_ID = 0;
+  /** Constants for the Belts system. */
+  public static class MailboxBelt {
+    /** The channel on the PCM for the belt motor */
+    public static final int BELT_MOTOR_ID = 0;
+    /** The speed for the belt motor. */
     public static final double BELT_MOTOR_SPEED = .7;
   }
-
+  /** Constants for the Controllers system. */
   public static class Controllers {
+    /** the channel on the computer for the driver controller. */
     public static final int DRIVER_CONTROLLER_PORT = 0;
   }
 }
