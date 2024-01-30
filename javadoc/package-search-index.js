8936d96cbacfe3b2ab5184c7e7d1f01183b26a3d
QuackitsQuinn <81661589+QuackitsQuinn@users.noreply.github.com>
2024-01-30 22:02:41 +0000

    Deploying to javadoc from @ frc937/robot2024@f94e44b12ad7e6b4c8ec412e1c954a51b45e60a0 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.commands"},{"l":"frc.robot.subsystems"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.commands"},{"l":"frc.robot.subsystems"},{"l":"frc.robot.subsystems.mailbox"}];updateSearchResults();
