a57836d4aba9aab08f4835eb791e6fd29e49a5ad
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-01-31 19:29:19 -0600

    Added FireNote.java

--- src/main/java/frc/robot/commands/mailbox/DeployPneumatics.java
+++ src/main/java/frc/robot/commands/mailbox/DeployPneumatics.java
@@ -38,6 +38,7 @@ public class DeployPneumatics extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    mailboxPneumatics.retract();
   }
 
   // Returns true when the command should end.
