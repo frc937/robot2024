89229341a7dd5123d110d7790ecced9e25f8a3e8
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-01-31 19:03:26 -0600

    removed redundant code

--- src/main/java/frc/robot/commands/mailbox/OutputNote.java
+++ src/main/java/frc/robot/commands/mailbox/OutputNote.java
@@ -34,11 +34,7 @@ public class OutputNote extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {
-    if (mailbox.getLimitSwitch()) {
-      
-    }
-  }
+  public void execute() {}
 
   // Called once the command ends or is interrupted.
   @Override
