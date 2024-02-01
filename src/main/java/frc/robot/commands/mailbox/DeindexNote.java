fed7601182dff64456a8878a2e3ddd3699072fb1
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-01-31 19:00:54 -0600

    DeindexNote runs the intake properly

--- src/main/java/frc/robot/commands/mailbox/DeindexNote.java
+++ src/main/java/frc/robot/commands/mailbox/DeindexNote.java
@@ -5,6 +5,7 @@
 package frc.robot.commands.mailbox;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotContainer;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.mailbox.Mailbox;
 
@@ -15,7 +16,9 @@ public class DeindexNote extends Command {
 
   /** Creates a new DeindexNote. */
   public DeindexNote() {
-    // Use addRequirements() here to declare subsystem dependencies.
+    this.intake = RobotContainer.intake;
+    this.mailbox = RobotContainer.mailbox;
+    addRequirements(intake, mailbox);
   }
 
   // Called when the command is initially scheduled.
@@ -24,11 +27,21 @@ public class DeindexNote extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    if (mailbox.getLimitSwitch()) {
+      intake.runIntake();
+    }
+    /* This else isn't neccessary, just advised for safety. If it interferes with anything, feel free to remove it. */
+    else {
+      intake.stop();
+    }
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    intake.stop();
+  }
 
   // Returns true when the command should end.
   @Override
