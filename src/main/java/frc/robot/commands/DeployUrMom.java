ff9f4efae3ef50fc1c069966780b652c860d820f
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-01-31 19:18:01 -0600

    javadoc for your mom

--- src/main/java/frc/robot/commands/DeployUrMom.java
+++ src/main/java/frc/robot/commands/DeployUrMom.java
@@ -14,6 +14,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.UrMom;
 
+/** Prints "your mom" */
 public class DeployUrMom extends Command {
   private UrMom urmom;
 
