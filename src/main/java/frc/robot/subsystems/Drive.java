cd6cc180f1b71fa94714cf95621755070e559ca1
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-02-10 12:08:44 -0600

    Added swerve lib to drive

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -14,12 +14,19 @@ package frc.robot.subsystems;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import java.io.File;
 import java.io.IOException;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
 
@@ -41,6 +48,33 @@ public class Drive extends SubsystemBase {
     }
     /* setting the motors to brake mode */
     drive.setMotorIdleMode(true);
+
+    // Configure AutoBuilder last
+    AutoBuilder.configureHolonomic(
+      this.drive::getPose, // Robot pose supplier
+      this.drive::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+      this.drive::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+      this.drive::setChassisSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+      new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+              new PIDConstants(1.0, 0.0, 0.0), // Translation PID constants
+              new PIDConstants(1.0, 0.0, 0.0), // Rotation PID constants
+              Constants.Drive.MAX_SPEED, // Max module speed, in m/s
+              Constants.Drive.MAX_ANGULAR_SPEED, // Drive base radius in meters. Distance from robot center to furthest module.
+              new ReplanningConfig() // Default path replanning config. See the API for the options here
+      ),
+      () -> {
+        // Boolean supplier that controls when the path will be mirrored for the red alliance
+        // This will flip the path being followed to the red side of the field.
+        // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent()) {
+          return alliance.get() == DriverStation.Alliance.Red;
+        }
+        return false;
+      },
+      this // Reference to this subsystem to set requirements
+    );
   }
 
   /**
