442a246919a3317b1772235c8cb9ec9e204129ca
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-02-10 12:34:12 -0600

    Added autoChooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,7 +11,11 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -111,11 +115,20 @@ public class RobotContainer {
   public static CommandXboxController driverController =
       new CommandXboxController(Constants.Controllers.DRIVER_CONTROLLER_PORT);
 
+  /** Sendable Chooser for path planning autos. */
+  private final SendableChooser<Command> autoChooser;
+
   /** Constructor for {@link RobotContainer} */
   public RobotContainer() {
     configureBindings();
 
     drive.setDefaultCommand(driveRobotOriented);
+
+    // Build an auto chooser. This will use Commands.none() as the default option.
+    autoChooser = AutoBuilder.buildAutoChooser();
+    // Another option that allows you to specify the default auto by its name
+    // autoChooser = AutoBuilder.buildAutoChooser("My Default Auto");
+    SmartDashboard.putData("Auto Chooser", autoChooser);
   }
 
   private void configureBindings() {
@@ -132,7 +145,7 @@ public class RobotContainer {
    * @return The current autonomous command.
    */
   public Command getAutonomousCommand() {
-    return Commands.print("No autonomous command configured");
+    return autoChooser.getSelected();
   }
 
   private static double scaleAxis(double axis) {
