a7409c8b15cbd66c62ac20a371212962514caa7a
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-02-12 17:08:28 -0600

    changed javadoc

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -75,16 +75,10 @@ public final class Constants {
     /** The distance from the center of the robot to any of the swerve modules. */
     public static double DISTANCE_ROBOT_CENTER_TO_SWERVE_MODULE = 0.3;
 
-    /**
-     * The Translation Drive PID for the robot. Since we aren't using drive PID this year, the
-     * values are set to the default.
-     */
+    /** The Translation Drive PID for the robot. */
     public static PIDConstants TRANSLATION_DRIVE_PID = new PIDConstants(1.0, 0.0, 0.0);
 
-    /**
-     * The Rotation Drive PID for the robot. Since we aren't using drive PID this year, the values
-     * are set to the default.
-     */
+    /** The Rotation Drive PID for the robot. */
     public static PIDConstants ROTATION_DRIVE_PID = new PIDConstants(1.0, 0.0, 0.0);
   }
 
