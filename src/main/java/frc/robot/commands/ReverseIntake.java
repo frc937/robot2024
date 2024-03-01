849349bda92cda090e477d450a5a2d9c5ad28490
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-02-29 19:05:23 -0600

    made reverse intake reverse in initialize and stop in end

--- src/main/java/frc/robot/commands/ReverseIntake.java
+++ src/main/java/frc/robot/commands/ReverseIntake.java
@@ -28,7 +28,9 @@ public class ReverseIntake extends Command {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    intake.reverseIntake();
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
@@ -36,7 +38,9 @@ public class ReverseIntake extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    intake.stop();
+  }
 
   // Returns true when the command should end.
   @Override
