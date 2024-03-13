9971e065e3991331723322d2da17eda246f62ee2
willitcode <91231142+willitcode@users.noreply.github.com>
2024-03-12 22:22:12 -0500

    make limelight end if it sees a target & loses it

--- src/main/java/frc/robot/commands/AimWithLimelight.java
+++ src/main/java/frc/robot/commands/AimWithLimelight.java
@@ -22,7 +22,7 @@ public class AimWithLimelight extends Command {
   private Drive drive;
   private Limelight limelight;
 
-  private boolean finished;
+  private boolean finished, hasSeenTarget;
   private int counter;
 
   private double steerStrength,
@@ -88,6 +88,7 @@ public class AimWithLimelight extends Command {
     this.oldPipelineNumber = limelight.getLimelightPipeline();
     limelight.setLimelightPipeline(pipelineNumber);
     this.finished = false;
+    this.hasSeenTarget = false;
     this.counter = 0;
   }
 
@@ -95,6 +96,7 @@ public class AimWithLimelight extends Command {
   @Override
   public void execute() {
     if (limelight.hasValidTarget()) {
+      hasSeenTarget = true;
       double z = limelight.getTX() * steerStrength;
       double yComponent =
           distanceFromTarget
@@ -117,6 +119,8 @@ public class AimWithLimelight extends Command {
           this.finished = true;
         }
       }
+    } else if (hasSeenTarget) {
+      this.finished = true;
     }
   }
 
