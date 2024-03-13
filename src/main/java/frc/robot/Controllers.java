0e742f493ca91411c6ba7255e7f81daa267ee50c
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-03-13 01:03:10 -0500

    assumed the boolean is true

--- src/main/java/frc/robot/Controllers.java
+++ src/main/java/frc/robot/Controllers.java
@@ -223,7 +223,7 @@ public final class Controllers {
   /** Rumbles the controllers while browning out */
   public static void rumbleIfBrowningOut() {
     if (RobotController.getBatteryVoltage() <= 10.5
-        && browningOutRumble.getBoolean(false)
+        && browningOutRumble.getBoolean(true)
         && !RobotController.isBrownedOut()) {
       rawOpXboxController.setRumble(GenericHID.RumbleType.kBothRumble, 0.5);
       rawPilotController.setRumble(GenericHID.RumbleType.kBothRumble, 0.5);
