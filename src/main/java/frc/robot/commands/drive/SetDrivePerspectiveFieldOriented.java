9b10409acbe8502946c636b12fa21cdf7f558dbb
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-03-20 17:45:33 -0500

    Sets the correct drive perspective

--- src/main/java/frc/robot/commands/drive/SetDrivePerspectiveFieldOriented.java
+++ src/main/java/frc/robot/commands/drive/SetDrivePerspectiveFieldOriented.java
@@ -20,7 +20,7 @@ public class SetDrivePerspectiveFieldOriented extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    drive.setDefaultCommand(RobotContainer.driveRobotOriented);
+    drive.setDefaultCommand(RobotContainer.driveFieldOriented);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
