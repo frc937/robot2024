1b5936fafb34f99a7b7399484ce8ab4986a359e2
willitcode <91231142+willitcode@users.noreply.github.com>
2024-03-20 19:40:54 -0500

    add heading getter method to drive

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -14,6 +14,7 @@ package frc.robot.subsystems;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.ReplanningConfig;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
@@ -179,6 +180,10 @@ public class Drive extends SubsystemBase {
    */
   public void setDriveMode(String driveMode) {
     driveLabelEntry.setString(driveMode);
+  }
+
+  public Rotation2d getHeading() {
+    return drive.getPose().getRotation();
   }
 
   /** Runs every scheduler run. */
