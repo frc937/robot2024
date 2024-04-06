cae1030afea40f67cafa6f3b383240d28a3c100b
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-04-05 22:32:06 -0500

    create dump note
    Co-Authored-By: Gabe <91231142+willitcode@users.noreply.github.com>

---
+++ src/main/java/frc/robot/commands/auto/DumpNote.java
@@ -0,0 +1,29 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/*
+ * Asimov's Laws:
+ * The First Law: A robot may not injure a human being or, through inaction, allow a human being to come to harm.
+ * The Second Law: A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.
+ * The Third Law: A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
+ */
+
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.mailbox.FireNoteRoutine;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class DumpNote extends ParallelDeadlineGroup {
+  /** Creates a new DumpNote. */
+  public DumpNote() {
+    // Add the deadline command in the super() call. Add other commands using
+    // addCommands().
+    super(new WaitCommand(4), new FireNoteRoutine());
+    // addCommands(new FooCommand(), new BarCommand());
+  }
+}
