783b30585e98a0af91a846f442757783693c8c64
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-04-07 16:46:44 -0500

    create drive to center auto

---
+++ src/main/java/frc/robot/commands/auto/DriveToCenterAuto.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/*
+ * Asimov's Laws:
+ * The First Law: A robot may not injure a human being or, through inaction, allow a human being to come to harm.
+ * The Second Law: A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.
+ * The Third Law: A robot must protect its own existence as long as such protection does not conflict with the First or Second Law.
+ */
+
+package frc.robot.commands.auto;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.drive.DriveFieldOrientedHeadingSnapping;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class DriveToCenterAuto extends ParallelCommandGroup {
+  /** Creates a new PickUpFromCenterAuto. */
+  public DriveToCenterAuto() {
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands(
+        new ParallelDeadlineGroup(
+            new WaitCommand(5),
+            new DriveFieldOrientedHeadingSnapping(
+                () -> 1.0,
+                () -> 0.0,
+                () -> 0.0,
+                () -> false,
+                () -> true,
+                () -> false,
+                () -> false)));
+  }
+}
