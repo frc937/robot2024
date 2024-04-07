0641695f3de35f3a2ec841c590f2c6882aa2d96f
Jack-Haefele <113944276+Jack-Haefele@users.noreply.github.com>
2024-04-07 16:46:31 -0500

    what is a parallel deadline group

--- src/main/java/frc/robot/commands/auto/DriveToAmpRed.java
+++ src/main/java/frc/robot/commands/auto/DriveToAmpRed.java
@@ -12,25 +12,37 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.drive.DriveFieldOrientedHeadingSnapping;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class DriveToAmpRed extends ParallelDeadlineGroup {
+public class DriveToAmpRed extends SequentialCommandGroup {
 
   /** Creates a new DriveToAmp. */
   public DriveToAmpRed() {
     super(
-        new WaitCommand(2),
-        new DriveFieldOrientedHeadingSnapping(
-            () -> -1.0,
-            () -> 0.0,
-            () -> 0.0,
-            () -> false,
-            () -> false,
-            () -> false,
-            () -> true)); /* TODO: CONSTANTS */
+        new ParallelDeadlineGroup(
+            new WaitCommand(1),
+            new DriveFieldOrientedHeadingSnapping(
+                () -> 0.0,
+                () -> 0.0,
+                () -> 0.0,
+                () -> false,
+                () -> false,
+                () -> false,
+                () -> true)),
+        new ParallelDeadlineGroup(
+            new WaitCommand(0.5),
+            new DriveFieldOrientedHeadingSnapping(
+                () -> 0.0,
+                () -> -1.0,
+                () -> 0.0,
+                () -> false,
+                () -> false,
+                () -> false,
+                () -> false))); /* TODO: CONSTANTS */
   }
 }
