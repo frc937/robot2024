02f53ab21629ccc5a96da2fadb1fdeecec8fda2a
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-04-22 16:28:12 -0500

    DumpNote javadoc

--- src/main/java/frc/robot/commands/auto/DumpNote.java
+++ src/main/java/frc/robot/commands/auto/DumpNote.java
@@ -18,6 +18,7 @@ import frc.robot.commands.mailbox.FireNoteRoutine;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+/** Auto that dumps the preloaded note in the robot after 4 seconds. */
 public class DumpNote extends ParallelDeadlineGroup {
   /** Creates a new DumpNote. */
   public DumpNote() {
