8afa2fd853b8301d5b8e1221d49c0e9fe739446c
Berdenson <91093973+Berdenson@users.noreply.github.com>
2024-04-22 16:30:39 -0500

    PickUpFromCenterAuto javadoc

--- src/main/java/frc/robot/commands/auto/PickUpFromCenterAuto.java
+++ src/main/java/frc/robot/commands/auto/PickUpFromCenterAuto.java
@@ -20,6 +20,7 @@ import frc.robot.commands.drive.DriveFieldOrientedHeadingSnapping;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+/** Auto that drives the bot to a note and intakes it. */
 public class PickUpFromCenterAuto extends ParallelCommandGroup {
   /** Creates a new PickUpFromCenterAuto. */
   public PickUpFromCenterAuto() {
