fda411694b40925e40854301e496c9e539414b4c
QuackItsQuinn <quinn.weismiller@gmail.com>
2025-02-04 16:17:36 -0600

    Fix Climber

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -11,8 +11,13 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -24,11 +29,12 @@ public class Climber extends SubsystemBase {
   public Climber() {
     this.climberMotor = new SparkMax(Constants.Climber.CLIMBER_MOTOR_ID, MotorType.kBrushed);
 
+    SparkMaxConfig climberConfig = new SparkMaxConfig();
+    climberConfig.idleMode(Constants.Climber.CLIMBER_MOTOR_IDLE_MODE);
+
     climberMotor.setInverted(Constants.Climber.CLIMBER_INVERTED);
 
-    climberMotor.setIdleMode(Constants.Climber.CLIMBER_MOTOR_IDLE_MODE);
-
-    climberMotor.burnFlash();
+    climberMotor.configure(climberConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   /** Tells the robot to climb up. */
